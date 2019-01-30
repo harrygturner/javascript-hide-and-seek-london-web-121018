16692f97b8ea46efbbcf84e98f5b4062a48bb271
github username <github email address>
2019-01-30 13:14:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+const nestedTarget = () => document.querySelector('#nested .target')
+
+const deepestChild = () => document.querySelector('#grand-node div div div div')
+
+function increaseRankBy(n) {
+  const list = document
+    .querySelectorAll('#app ul.ranked-list li');
+  for (let i = 0; i < list.length; i++) {
+    let num = parseInt(list[i].innerHTML);
+    list[i].innerHTML = (num + n).toString();
+  }
+}
